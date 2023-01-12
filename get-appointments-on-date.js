8e1e28b2271fb7362c11e1503c5f8802bd541a14
Linda Chen <lindachenau@gmail.com>
2023-01-12 15:22:46 +1100

    Added recordstatus to appointment

--- get-appointments-on-date.js
+++ get-appointments-on-date.js
@@ -5,7 +5,7 @@ const { bpConfig } = require("./bp-config")
 const getAppointmentsOnDate = async (date) => {
   try {
     const pool = await sql.connect(bpConfig)
-    const queryStr = `SELECT Appointments.RECORDID,Appointments.APPOINTMENTTYPE,Appointments.APPOINTMENTDATE,Appointments.APPOINTMENTTIME,Patients.FIRSTNAME,Patients.SURNAME,Patients.MOBILEPHONE from Appointments LEFT JOIN Patients ON Appointments.INTERNALID=Patients.INTERNALID where Appointments.APPOINTMENTDATE=\'${date}\' and Appointments.RECORDSTATUS in (1,4)`
+    const queryStr = `SELECT Appointments.RECORDSTATUS,Appointments.RECORDID,Appointments.APPOINTMENTTYPE,Appointments.APPOINTMENTDATE,Appointments.APPOINTMENTTIME,Patients.FIRSTNAME,Patients.SURNAME,Patients.MOBILEPHONE from Appointments LEFT JOIN Patients ON Appointments.INTERNALID=Patients.INTERNALID where Appointments.APPOINTMENTDATE=\'${date}\' and Appointments.RECORDSTATUS in (1,4)`
     const result = await pool.request()
       .query(queryStr)
     
