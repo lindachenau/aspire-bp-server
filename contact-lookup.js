804a63578b95300769f3eaf57421150002c155e6
Linda Chen <lindachenau@gmail.com>
2023-10-02 16:54:46 +1100

    trimed trailing spaces of patient lookup by phone

--- contact-lookup.js
+++ contact-lookup.js
@@ -5,14 +5,25 @@ const contactByPhone = async (phone) => {
   try {
     const pool = await sql.connect(bpConfig)
 
-    const queryStr = `SELECT INTERNALID, FIRSTNAME, PREFERREDNAME, SURNAME, CITY FROM Patients WHERE MOBILEPHONE LIKE \'${phone}\' AND RECORDSTATUS = 1`
+    const queryStr = `SELECT INTERNALID, FIRSTNAME, PREFERREDNAME, SURNAME, CITY, MOBILEPHONE FROM Patients WHERE MOBILEPHONE LIKE \'${phone}\' AND RECORDSTATUS = 1`
     const result = await pool.request()
       .query(queryStr)
     
-    if (result.recordset.length > 0)
-      return result.recordset[0]
-    else
+    if (result.recordset.length > 0) {
+      const contact = {
+        id: result.recordset[0].INTERNALID,
+        firstname: result.recordset[0].FIRSTNAME.trim(),
+        lastname: result.recordset[0].SURNAME.trim(),
+        preferredname: result.recordset[0].PREFERREDNAME.trim(),
+        city: result.recordset[0].CITY.trim(),
+        mobilephone: result.recordset[0].MOBILEPHONE.trim()
+      }
+      
+      return contact
+    }
+    else {
       return {}
+    }
 
   } catch (err) {
     console.log(err)
