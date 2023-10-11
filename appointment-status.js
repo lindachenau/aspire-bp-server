f0cefb96f62e3848be626813810870913b034cb8
Linda Chen <lindachenau@gmail.com>
2023-10-11 19:02:46 +1100

    Added record status to detect cancelled appointment

--- appointment-status.js
+++ appointment-status.js
@@ -1,15 +1,31 @@
 const sql = require('mssql');
 const { runStoredProcedure } = require("./util")
 const { bpConfig } = require("./bp-config")
+const moment = require('moment')
 
 const appointmentStatus = async (aptID) => {
 
   const params = [{ "name": "AptID", "type": sql.Int, "value": aptID }];
   
   const pool = await sql.connect(bpConfig)
-  const result = await runStoredProcedure(pool, 'BP_GetAppointmentDetails', params)
+  const result1 = await runStoredProcedure(pool, 'BP_GetAppointmentDetails', params)
+  const details = result1.recordset[0]
+  const apt = {
+    patient: `${details.Firstname.trim()} ${details.Surname.trim()}`,
+    aptDate: moment(new Date(details.AppointmentDate)).format("YYYY-MM-DD"),
+    aptTime: aptTimeString(details.AppointmentTime),
+    aptType: details.AppointmentType,
+    aptDescription: details.Description.trim(),
+    provider: `${details.UserTitle.trim()} ${details.UserFirstname.trim()} ${details.UserSurname.trim()}`
+  }
 
-  return result.recordset[0]
+  const queryStr = `SELECT Appointments.RECORDSTATUS from Appointments where Appointments.RECORDID=${aptID}`
+  const result2 = await pool.request()
+    .query(queryStr)  
+
+  apt.status = result2.recordset[0].RECORDSTATUS  
+  
+  return apt
 }
 
 // (async () => {
